74919084d7725984108e94c2fe200804cf318f8d
James Cowan <jamesmcowan@googlemail.com>
2021-02-17 09:42:29 +0000

    handle other fields

--- src/ts/view/ProfileDialog.tsx
+++ src/ts/view/ProfileDialog.tsx
@@ -17,26 +17,67 @@ export interface IProfileDialogProps {
 
 interface IProfileState {
     edit: boolean;
-    firstName: string;
-    otherName: string;
     user: IUser;
 }
+
+interface IField {
+    label: string;
+    id: string;
+    parent: any;
+}
+
+const fields: IField[] = [
+    { label: "First Name", id: 'first_name', parent: null },
+    { label: "Other Names", id: 'other_names', parent: null },
+    { label: "Town", id: 'town', parent: "address" },
+    { label: "County", id: 'county', parent: "address" },
+    { label: "PostCode", id: 'postcode', parent: "address" },
+    { label: "Mobile", id: 'mobile', parent: null },
+    { label: "Email", id: 'email', parent: null },
+    { label: "Company", id: 'company', parent: null },
+    { label: "Contact", id: 'contact', parent: "preferences" }
+]
 
 export function ProfileDialog(props: IProfileDialogProps) {
     const [state, setState] = useState<IProfileState>(initialState());
 
+    function action() {
+        if (state.edit) {
+            save();
+        }
+        else {
+            edit();
+        }
+    }
+
+    function cancel() {
+        if (state.edit) {
+            cancelEdit();
+        }
+        else {
+            props.close();
+        }
+    }
+
     function cancelEdit() {
-        let user:IUser = props.dataService.getUser();
+        let user: IUser = props.dataService.getUser();
 
-        setState({ ...state, edit: false, firstName: user.first_name, otherName: user.other_names});
+        setState({ ...state, edit: false, user: Object.assign({}, user) });
     }
 
-    function changeFirstName(e) {
-        setState({ ...state, firstName: e.target.value });
-    }
+    function changeField(e, field) {
+        let user: IUser = state.user;
+        let id = field.id;
+        let parent = field.parent;
 
-    function changeOtherName(e) {
-        setState({ ...state, otherName: e.target.value });
+        if (parent) {
+            user[parent][id] = e.target.value;
+        }
+        else {
+            user[id] = e.target.value;
+        }
+    
+        setState({ ...state, user: user });
     }
 
     function edit() {
@@ -44,58 +85,44 @@ export function ProfileDialog(props: IProfileDialogProps) {
     }
 
     function initialState(): IProfileState {
-        let user:IUser = props.dataService.getUser();
+        let user: IUser = props.dataService.getUser();
 
-        return { edit: false, firstName: user.first_name, otherName: user.other_names, user: user };
+        return { edit: false, user: Object.assign({}, user) };
     }
 
     function save() {
-        let user:IUser = props.dataService.getUser();
-
-        user.first_name = state.firstName;
-        user.other_names = state.otherName;
-        props.dataService.saveUser(user);
+        props.dataService.saveUser(state.user);
         props.close();
     }
 
+    let actionButtonTitle = (state.edit) ? "Save" : "Edit";
+
     return (
         <div>
-            {!state.edit &&
-                <Dialog
-                    fullWidth={false}
-                    maxWidth="md"
-                    open={true}
-                    aria-labelledby="save-dialog-title"
-                    aria-describedby="save-dialog-description">
-                    <DialogTitle id="save-dialog-title">View</DialogTitle>
-                    <DialogContent dividers>
-                        <TextField autoFocus margin="dense" disabled={true} fullWidth variant="outlined" value={state.firstName} />
-                        <TextField autoFocus margin="dense" disabled={true} fullWidth variant="outlined" value={state.otherName} />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button color="primary" autoFocus onClick={() => { props.close() }}>Cancel</Button>
-                        <Button color="primary" autoFocus onClick={() => { edit() }}>Edit</Button>
-                    </DialogActions>
-                </Dialog>
-            }
-            {state.edit &&
-                <Dialog
-                    fullWidth={false}
-                    maxWidth="md"
-                    open={true}
-                    aria-labelledby="save-dialog-title"
-                    aria-describedby="save-dialog-description">
-                    <DialogTitle id="save-dialog-title">Edit</DialogTitle>
-                    <DialogContent dividers>
-                        <TextField autoFocus margin="dense" fullWidth onChange={changeFirstName}  variant="outlined" value={state.firstName} />
-                        <TextField autoFocus margin="dense" fullWidth onChange={changeOtherName} variant="outlined" value={state.otherName} />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button color="primary" autoFocus onClick={() => { cancelEdit() }}>Cancel</Button>
-                        <Button color="primary" autoFocus onClick={() => { save() }}>Save</Button>
-                    </DialogActions>
-                </Dialog>
-            }
+            <Dialog
+                fullWidth={false}
+                maxWidth="md"
+                open={true}
+                aria-labelledby="save-dialog-title"
+                aria-describedby="save-dialog-description">
+                <DialogTitle id="save-dialog-title">Edit</DialogTitle>
+                <DialogContent dividers>
+                    {
+                        fields.map((field: IField, index) => {
+                            let id = field.id;
+                            let parent = field.parent;
+                            let value = (parent) ? state.user[parent][id]: state.user[id];
+                            let placeholder = "Enter "+field.label;
+
+                            return <TextField autoFocus placeholder={placeholder} margin="dense" disabled={!state.edit} fullWidth label={field.label} onChange={(e) => { changeField(e, field) }} variant="outlined" value={value} />
+                        })
+                    }
+                </DialogContent>
+                <DialogActions>
+                    <Button color="primary" autoFocus onClick={() => { cancel() }}>Cancel</Button>
+                    <Button color="primary" autoFocus onClick={() => { action() }}>{actionButtonTitle}</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 
